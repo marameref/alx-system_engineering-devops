0290a085146a8f38a7de9a3d1fbc847b0d1e86c0
marameref <mirrorprojectsltd@gmail.com>
2023-05-09 14:34:13 +0100

    Task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,30 +1,54 @@
-import praw
+#!/usr/bin/python3
+""" raddit api"""
 
-def count_words(subreddit, word_list, limit=10, count_dict=None, after=None):
-    # Create a new Reddit instance
-    reddit = praw.Reddit(client_id='YOUR_CLIENT_ID',
-                         client_secret='YOUR_CLIENT_SECRET',
-                         user_agent='YOUR_USER_AGENT')
+import json
+import requests
 
-    # Initialize the count dictionary if it doesn't exist
-    if count_dict is None:
-        count_dict = {}
 
-    # Get the hot articles from the subreddit
-    hot_articles = reddit.subreddit(subreddit).hot(limit=limit, params={"after": after})
+def count_words(subreddit, word_list, after="", count=[]):
+    """count all words"""
 
-    # Loop through each article and count the occurrences of each word in the title
-    for article in hot_articles:
-        title = article.title.lower()
-        for word in word_list:
-            if word.lower() in title and 'java' not in title:
-                count_dict[word] = count_dict.get(word, 0) + 1
+    if after == "":
+        count = [0] * len(word_list)
 
-    # If there are more articles, recursively call the function with the after parameter set
-    if hot_articles.after is not None:
-        count_words(subreddit, word_list, limit=limit, count_dict=count_dict, after=hot_articles.after)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'user-agent': 'bhalut'})
 
-    # Once all articles have been processed, print the sorted count dictionary
-    sorted_count = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
-    for word, count in sorted_count:
-        print(f"{word}: {count}")
+    if request.status_code == 200:
+        data = request.json()
+
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        count[i] += 1
+
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
+
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
+
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
+        else:
+            count_words(subreddit, word_list, after, count)
